b974989071ccdf8982bfddeb23ecc725be26a3c8
Inejka <saske1265@bk.ru>
2024-11-05 22:24:13 +0300

    feat: add maps patching

--- translate.py
+++ translate.py
@@ -6,10 +6,12 @@ from game_types import Actor, Armor, Class, Enemy, Item, State, Weapon, Skill, s
 import shutil
 from pathlib import Path
 
+MAPS_ATTENSION = "translate_me_maps"
 SOURCE_FOLDER_JAP = "demo_jap"
 SOURCE_FOLDER_TRANSLATION = "demo_rus"
 FOLDER_TO_PATCH = "jap_3_01"
 TRANSLATION_FOLDER = "translation_files"
+MAPS_FOLDER = "Maps"   
 
 def check_percentage(old_items, new_items):
     found = 0       
@@ -122,9 +124,149 @@ def move_translation_system_files():
     else:
         print(f"System.txt NOT COPIED, CHECK SIMULARITY: {simularity_score} AND TRANSLATE IT MANUALLY, SYSTEM WILL NOT BE COMPILED")
 
+def move_translation_files_maps():
+    useful_words = ["ShowText(", "Display Name", "ShowChoices", "display_skill_name", "ScriptMore", "unlimited_choices", "ex_choice_add", "When"]
+
+    def keep_line(line):
+        for word in useful_words:
+            if word in line:
+                return True
+        return False
+    
+    vocabulary = {}
+    simple_vocabulary = {}
+
+    def is_end_of_chunk(text, inx, context_length):
+        for i in range(inx, min(len(text), inx + context_length + 1)):
+            if keep_line(text[i]):
+                return False
+        return True
+
+
+    def break_text_into_chunks(text, context_length=3):
+        chunks = []
+        text = text.split("\n")
+        i = 0
+        while i < len(text):
+            if keep_line(text[i]):
+                start_of_chunk = i
+                j = i + 1
+                while not is_end_of_chunk(text, j, context_length):
+                    j += 1
+                chunks.append(("\n".join(text[i-context_length:start_of_chunk]), "\n".join(text[start_of_chunk:j]), "\n".join(text[j:j+context_length])))
+                i = j
+            else:
+                i += 1
+        return chunks
+            
+    def break_chunk(text):
+        text = text.split("\n")
+        first = 0
+        last = 0
+        while last < len(text):
+            if not keep_line(text[last]):
+                yield "\n".join(text[first:last])
+                while last < len(text) and not keep_line(text[last]):
+                    last += 1
+                first = last
+                last += 1
+            else:
+                last += 1
+        yield "\n".join(text[first:last])
+
+    def process_chunks(chunks, translated_text):
+        context_was_not_founded = False
+        last_context_position = 0
+        for chunk in chunks:
+            start_position = translated_text.find(chunk[0], last_context_position)
+            if start_position != -1:
+                start_position += len(chunk[0])
+            end_position = translated_text.find(chunk[2], start_position)           
+            if start_position != -1 and end_position != -1:
+                for source, target in zip(break_chunk(chunk[1]), break_chunk(translated_text[start_position+1: end_position-1])):
+                    vocabulary[source] = target
+            else:
+                context_was_not_founded = True
+
+            last_context_position = start_position
+        return context_was_not_founded
+
+    def create_simple_shunks(text):
+        chunks = []
+        lines = text.split("\n")
+        start_position = 0
+        end_position = 0
+        i = 0
+        while i < len(lines):
+            if keep_line(lines[i]):
+                start_position = i
+                end_position = i + 1
+                while end_position < len(lines) and keep_line(lines[end_position]):
+                    end_position += 1
+                chunks.append("\n".join(lines[start_position:end_position]))
+                i = end_position + 1
+            else:
+                i += 1
+        return chunks
+            
+
+    for item in Path(f"{SOURCE_FOLDER_JAP}/{MAPS_FOLDER}").glob('**/*.txt'):
+        source_text = ""
+        translated_text = ""
+        with open(item, 'r', encoding='utf-8') as source_file:
+            source_text = source_file.read()
+        with open(str(item).replace(SOURCE_FOLDER_JAP, SOURCE_FOLDER_TRANSLATION), 'r', encoding='utf-8') as translate_file:
+            translated_text = translate_file.read()
+        chunks = break_text_into_chunks(source_text)
+        process_chunks(chunks, translated_text)
+
+        jap_simple_chunks = create_simple_shunks(source_text)
+        rus_simple_chunks = create_simple_shunks(translated_text)
+        if len(jap_simple_chunks) == len(rus_simple_chunks):
+            for jap, rus in zip(jap_simple_chunks, rus_simple_chunks):
+                simple_vocabulary[jap] = rus
+
+
+    print(f"Len of simple vocabulary {len(simple_vocabulary)}")
+    print(f"Len of complicated vocabulary {len(vocabulary)}")
+ 
+    os.makedirs(f"{TRANSLATION_FOLDER}/{MAPS_FOLDER}/Map/Data", exist_ok=True)
+    os.makedirs(f"{TRANSLATION_FOLDER}/{MAPS_FOLDER}/Map2/Data", exist_ok=True)
+    os.makedirs(f"{TRANSLATION_FOLDER}/{MAPS_ATTENSION}/Map/Data", exist_ok=True)
+    os.makedirs(f"{TRANSLATION_FOLDER}/{MAPS_ATTENSION}/Map2/Data", exist_ok=True)
+
+
+    full_patched_maps = 0
+    total_maps = 0
+    with open("nigger.txt", 'w', encoding='utf-8') as file:
+        file.write(str(vocabulary))
+    for item in Path(f"{FOLDER_TO_PATCH}/{MAPS_FOLDER}").glob('**/*.txt'):
+        succesfully_patched = True
+        text = item.read_text(encoding='utf-8')
+        chunks = break_text_into_chunks(text)
+        total_maps += 1
+        for chunk in chunks:
+            for piece in break_chunk(chunk[1]):
+                if piece in simple_vocabulary:
+                    text = text.replace(piece, simple_vocabulary[piece])
+                elif piece in vocabulary:
+                    text = text.replace(piece, vocabulary[piece])
+                else:
+                    succesfully_patched = False
+
+        if succesfully_patched:
+            with open(str(item).replace(FOLDER_TO_PATCH, TRANSLATION_FOLDER), 'w', encoding="utf-8") as transition_file:
+                transition_file.write(text)
+            full_patched_maps += 1
+        else:
+            with open(str(item).replace(FOLDER_TO_PATCH, TRANSLATION_FOLDER).replace(MAPS_FOLDER,MAPS_ATTENSION), 'w', encoding="utf-8") as transition_file:
+                transition_file.write(text)
+
+    print(f"Maps fully patched {full_patched_maps} / {total_maps}")
+
 
 
 if __name__ == "__main__":
-    move_translation_files()
-    move_translation_system_files()
-    # move_translation_files_maps()
+    # move_translation_files()
+    # move_translation_system_files()
+    move_translation_files_maps()
